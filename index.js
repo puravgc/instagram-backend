974edec5040b09beddd3bd51b4b5635b8930e755
puravgc <puravgc02@gmail.com>
2024-06-06 14:34:30 +0545

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 5000;
+const port = 5000;
 const cors = require("cors");
 const mongoose = require("mongoose");
 const UserModel = require("./models/model");
